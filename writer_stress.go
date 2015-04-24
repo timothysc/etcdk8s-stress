49d689d4844e7b0f3ab5a1649c3fa63e24a19d1f
Timothy St. Clair <tstclair@redhat.com>
2015-04-24 15:19:36 -0500

    Update to copy k8's flow

--- writer_stress.go
+++ writer_stress.go
@@ -8,10 +8,37 @@ import (
 	"github.com/coreos/go-etcd/etcd"
 )
 
+// copying the exact k8's flow
+type EtcdGetSet interface {
+	GetCluster() []string
+	Get(key string, sort, recursive bool) (*etcd.Response, error)
+	Set(key, value string, ttl uint64) (*etcd.Response, error)
+	Create(key, value string, ttl uint64) (*etcd.Response, error)
+	Delete(key string, recursive bool) (*etcd.Response, error)
+	CompareAndSwap(key, value string, ttl uint64, prevValue string, prevIndex uint64) (*etcd.Response, error)
+	Watch(prefix string, waitIndex uint64, recursive bool, receiver chan *etcd.Response, stop chan bool) (*etcd.Response, error)
+}
+
+func writer(client EtcdGetSet, prefix string, interval time.Duration, c int, i int) {
+	time.Sleep(time.Duration(int(interval) / c * i))
+	for {
+		key := fmt.Sprintf("%s/%v", prefix, i)
+		_, err := client.Set(key, key, 0)
+		if err != nil {
+			log.Printf("error setting key %s", key)
+			continue
+		}
+		log.Printf("set key %s", key)
+		time.Sleep(interval)
+	}
+}
+
 func stressWrite(endpoints []string, c int, prefix string, interval time.Duration) {
+	var client EtcdGetSet
+	client = etcd.NewClient(endpoints)
 	for i := 0; i < c; i++ {
-		client := etcd.NewClient(endpoints)
-		go func(i int) {
+		go writer(client, prefix, interval, c, i)
+		/*go func(i int) {
 			time.Sleep(time.Duration(int(interval) / c * i))
 			for {
 				key := fmt.Sprintf("%s/%v", prefix, i)
@@ -23,6 +50,6 @@ func stressWrite(endpoints []string, c int, prefix string, interval time.Duratio
 				log.Printf("set key %s", key)
 				time.Sleep(interval)
 			}
-		}(i)
+		}(i)*/
 	}
 }
